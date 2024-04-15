4896890d00ff42a0a3cfb29b9daba772ae89c858
Matheo Breugnot <matheo9477@gmail.com>
2024-04-15 16:32:10 +0200

    pas reussi

--- FrontEnd/Projet.js
+++ FrontEnd/Projet.js
@@ -1,14 +1,25 @@
-fetch("http://localhost:5678/api/works");
+let reponse = 0
+let data = 0
 
+async function RecupWorks() {
+    try {
+        let reponse = await fetch("http://localhost:5678/api/works");
+        let data = await reponse.json();
+        console.log(data);
+    } catch (error) {
+        console.log("error");
+    }
+}
 
+RecupWorks();
 
-
-let contenuCaption = "Abajour Tahina"
+let contenuCaption = data.title
+let sourceimg = data.imageUrl
 
 let figure = `
        
         <figure>
-            <img src="assets/images/abajour-tahina.png" alt="Abajour Tahina">
+            <img src= ${sourceimg} alt= ${contenuCaption}>
             <figcaption>${contenuCaption}</figcaption>
         </figure>
        
