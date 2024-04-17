a7fb51eacae3e6861649b9a41d807c3afdafd276
Matheo Breugnot <matheo9477@gmail.com>
2024-04-17 15:01:00 +0200

    Terminé Etape1.1

--- FrontEnd/Projet.js
+++ FrontEnd/Projet.js
@@ -1,30 +1,40 @@
-let reponse = 0
-let data = 0
-
 async function RecupWorks() {
-    try {
-        let reponse = await fetch("http://localhost:5678/api/works");
-        let data = await reponse.json();
-        console.log(data);
-    } catch (error) {
-        console.log("error");
+    const reponse = await fetch("http://localhost:5678/api/works/");
+    const works = await reponse.json();
+    console.log(works);
+
+    for (let i = 0; i < works.length; i++) {
+        console.log(works[i].title)
+
+        const gallery = document.querySelector(".gallery");             //Récupération DOM gallerie//
+
+        const figure = document.createElement("figure");                //Création balise figure//
+
+        const image = document.createElement("img");                    //Création balise img avec source et alt//
+        image.src = works[i].imageUrl;
+        image.alt = works[i].title;
+
+        const caption = document.createElement("figcaption");           //Création balise figcaption avec titre//
+        caption.innerText = works[i].title;
+
+
+        gallery.appendChild(figure)                              
+        figure.appendChild(image)                                       //Implémentation dans le DOM//
+        figure.appendChild(caption)
     }
+    
 }
 
 RecupWorks();
 
-let contenuCaption = data.title
-let sourceimg = data.imageUrl
-
-let figure = `
-       
-        <figure>
-            <img src= ${sourceimg} alt= ${contenuCaption}>
-            <figcaption>${contenuCaption}</figcaption>
-        </figure>
-       
-    `;
 
 
-    let gallery = document.querySelector(".gallery")
-    gallery.innerHTML = figure
+
+
+
+
+
+
+
+
+
